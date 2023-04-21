0a4aa8662c737f7c36312cb4935d345808edf1ce
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-04-21 16:57:52 -0600

    feat: set db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '0CZ1kn39zHuzvg@2',
+      database: 'tasks-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, TasksController],
   providers: [AppService, TasksService],
 })
