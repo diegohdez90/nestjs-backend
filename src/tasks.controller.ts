d9017fade93e53625ce909de92ddf5715dfdb2f7
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-04-21 12:06:33 -0600

    feat: add retreive task by id

--- src/tasks.controller.ts
+++ src/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import Task from './task.model';
 import { CreateTaskDtp } from './dto/create-task.dto';
@@ -15,5 +15,10 @@ export class TasksController {
   @Post()
   create(@Body() createTaskDtp: CreateTaskDtp): Task {
     return this.tasksService.create(createTaskDtp);
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string) {
+    return this.tasksService.getById(id);
   }
 }
