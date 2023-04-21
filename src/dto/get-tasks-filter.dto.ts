c6f48277d2323b86931e299fd6f3d4c714c9f16d
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-04-21 13:41:41 -0600

    feat: add validator to filter data

--- src/dto/get-tasks-filter.dto.ts
+++ src/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,10 @@
+import { IsOptional, IsEnum } from 'class-validator';
 import { TaskStatus } from 'src/task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
   search?: string;
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
 }
