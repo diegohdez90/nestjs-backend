d3a4343ed288ed10ac71f898d99b695387414c3a
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-04-24 13:53:10 -0600

    feat: add validation for user scheme

--- src/dto/auth-credentials.dto.ts
+++ src/dto/auth-credentials.dto.ts
@@ -1,4 +1,15 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MaxLength(24)
+  @MinLength(6)
   username: string;
+  @IsString()
+  @MaxLength(32)
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Your password is weak',
+  })
   password: string;
 }
